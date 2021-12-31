85c8624472780ceca523c0aef1f038efb3ffedba
NurulIslam-EEE <ni163034@gmail.com>
2021-12-31 23:17:41 +0600

    commenting on code

--- sort-user.js
+++ sort-user.js
@@ -8,7 +8,7 @@ let arr = [
     { name: 'Rana', age: 31, temperature: 98 },
     { name: 'Rana', age: 88, temperature: 98 },
     { name: 'sunil', age: 20, temperature: 98 },
-    { name: 'Biplap', age: 22, temperature: 98 },
+    { name: 'Biplap', age: 31, temperature: 98 },
     { name: 'Rana', age: 35, temperature: 98 },
     { name: 'sunil', age: 20, temperature: 98 },
     { name: 'Biplap', age: 30, temperature: 98 },
@@ -26,11 +26,12 @@ function vaxTrail(numbers) {
     let x = [];
     let y = [];
     let z = [];
-
+    //looping the given array
     numbers.map(a => {
+
         if (a.age >= 20 && a.age <= 30) {
             m.push(a)
-            console.log(m)
+
         } else if (a.age >= 31 && a.age <= 40) {
             x.push(a)
         } else if (a.age >= 41 && a.age <= 50) {
@@ -39,6 +40,7 @@ function vaxTrail(numbers) {
             z.push(a)
         }
     })
+    //sorting small age to big 
     const i = m.sort((a, b) => a.age - b.age);
     result2.A = i;
     const j = x.sort((a, b) => a.age - b.age);
@@ -53,4 +55,7 @@ function vaxTrail(numbers) {
 
 const result = vaxTrail(arr)
 console.log(result);
+
+
+
 
